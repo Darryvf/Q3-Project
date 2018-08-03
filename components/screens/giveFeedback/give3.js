c46d607dcdf071f9d92910a2b8740ef0b67d7f52
Melissa Pizarro <mpizarro03@gmail.com>
2018-08-02 20:12:46 -0600

    linked gives1-6 routes, rearanged frown/smile icons in gives4-6 to match 10 is smile and 0 is frown

---
+++ components/screens/giveFeedback/give3.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react'
+import { Text, View, Image, Slider, TextInput } from 'react-native'
+import ButtonElement from '../../elements/button.js'
+import Styles from '../../styles.js'
+
+class Give3 extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      value: 0
+    }
+  }
+
+  static navigationOptions = {
+   header: null
+ }
+
+ sliderChange(value) {
+   this.setState(() => {
+      return {
+        value: parseFloat(value),
+      }
+    })
+ }
+
+  render() {
+    const { navigate } = this.props.navigation
+    const {value} = this.state
+    return (
+      <View style={Styles.container}>
+        <View style={Styles.header}></View>
+        <View style={Styles.body}>
+          <View style={Styles.giveFeedback}>
+            <View style={Styles.giveHeader}>
+              <Text style={Styles.h2}>How did your partner do on
+                <Text style={Styles.highlight}> Alone Time </Text>
+
+              this week?
+              </Text>
+            </View>
+
+            <View style={Styles.spacerLarge}></View>
+
+            <View style={Styles.giveScore}>
+              <Image
+                style={Styles.smile}
+                source={require('../../../assets/img/frown.png')}
+              />
+              <Text style={Styles.h2}>{this.state.value}</Text>
+              <Image
+                style={Styles.smile}
+                source={require('../../../assets/img/smile.png')}
+              />
+            </View>
+
+            <View style={Styles.spacerSmall}></View>
+
+            <View style ={Styles.sliderContainer}>
+              <Slider
+                // maximumTrackTintColor="pink"
+                minimumTrackTintColor="#30E7CE"
+                step={1}
+                maximumValue={10}
+                onValueChange={this.sliderChange.bind(this)}
+                value={value}>
+              </Slider>
+            </View>
+
+            <View style={Styles.spacerLarge}></View>
+
+            <View style={Styles.giveCenter}>
+              <TextInput
+                multiline={true}
+                placeholder='What could they have done to get a better score?'
+                style={Styles.addDescription} />
+            </View>
+
+          <View style={Styles.spacerLarge}></View>
+
+            <View style={Styles.giveCenter}>
+                <ButtonElement
+                  buttonText="Save"
+                  press={navigate}
+                  screen="Give4"
+                  onValueChange={(e) => this.sliderChange(e.target.value)}/>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default Give3
