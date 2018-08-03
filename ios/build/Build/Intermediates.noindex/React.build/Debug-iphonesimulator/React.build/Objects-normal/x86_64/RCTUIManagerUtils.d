f29b0c07450004533c6ac2c48999114ea374d018
Melissa Pizarro <mpizarro03@gmail.com>
2018-08-02 18:58:37 -0600

    adding other people's builds

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTUIManagerUtils.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTUIManagerUtils.d
@@ -1,5 +1,5 @@
 dependencies: \
-  /Users/aimeezawacki/code/q3/Q3-project/node_modules/react-native/React/Modules/RCTUIManagerUtils.m \
-  /Users/aimeezawacki/code/q3/Q3-project/node_modules/react-native/React/Modules/RCTUIManagerUtils.h \
-  /Users/aimeezawacki/code/q3/Q3-project/node_modules/react-native/React/Base/RCTAssert.h \
-  /Users/aimeezawacki/code/q3/Q3-project/node_modules/react-native/React/Base/RCTDefines.h
+  /Users/mpizarro34/G89/workspace/q3/q3Project/Q3-Project/node_modules/react-native/React/Modules/RCTUIManagerUtils.m \
+  /Users/mpizarro34/G89/workspace/q3/q3Project/Q3-Project/node_modules/react-native/React/Modules/RCTUIManagerUtils.h \
+  /Users/mpizarro34/G89/workspace/q3/q3Project/Q3-Project/node_modules/react-native/React/Base/RCTAssert.h \
+  /Users/mpizarro34/G89/workspace/q3/q3Project/Q3-Project/node_modules/react-native/React/Base/RCTDefines.h
